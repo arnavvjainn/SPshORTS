e8af3788cbd28795517bbd01fb13aab59c670d63
arnavvjainn <jain.arnavv@gmail.com>
2024-01-05 16:20:59 +0530

    working backend

---
+++ backend/app.py
@@ -0,0 +1,34 @@
+from distutils.log import debug
+from multiprocessing import context
+from flask import Flask, render_template 
+from newsapi import NewsApiClient
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    newsapi = NewsApiClient(api_key="eb76e62622bf4273bddd3ec57dd2e03b")
+    top_headlines = newsapi.get_top_headlines(category='sports', language='en')
+
+    articles = top_headlines['articles']
+
+    description = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        my_article = articles[i]
+        
+        news.append(my_article['title'])
+        description.append(my_article['description'])
+        img.append(my_article['urlToImage']) 
+
+        
+    my_list = zip(news, description, img) 
+
+    return render_template('index.html', context = my_list)
+
+if __name__ == "main":
+    app.run(debug=True)
