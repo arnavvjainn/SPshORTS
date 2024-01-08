89596da61cf5b1e08e50d4199fbb3848b140376e
arnavvjainn <jain.arnavv@gmail.com>
2024-01-08 21:07:18 +0530

    it is SUMMARIZINGGG

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,9 @@ from newsapi import NewsApiClient
 import config
 import requests
 from bs4 import BeautifulSoup
+import cohere
 
+co = cohere.Client(config.cohere_api_key)
 app = Flask(__name__)
 
 
@@ -35,9 +37,19 @@ app = Flask(__name__)
 
 #     return render_template('index.html', context = my_list)
 
+def summarize(text):
+    response = co.summarize(
+        text=text,
+        model='command',
+        length='medium',
+        extractiveness='high'
+    )
+    return response.summary
+
 @app.route('/', methods=['GET'])
 def index():
-    url = 'https://newsapi.org/v2/top-headlines?language=en&category=sports&apiKey='
+    url = f"https://newsapi.org/v2/top-headlines?language=en&category=sports&apiKey={config.news_api_key}"
+    #url = f"https://newsapi.org/v2/everything?q=bitcoin&apiKey={config.news_api_key}"
     response = requests.get(url)
     data = response.json()
 
@@ -47,7 +59,7 @@ def index():
     image_urls = []
 
     # Loop through articles and store details
-    for article in data['articles']:
+    for article in data['articles'][:2]:
         titles.append(article['title'])
         image_urls.append(article.get('urlToImage'))
 
@@ -55,8 +67,12 @@ def index():
         article_response = requests.get(article['url'])
         soup = BeautifulSoup(article_response.text, 'html.parser')
 
+        article_content = soup.get_text()
+
+        summarized_content = summarize(article_content)
+
         # Extract text and store
-        contents.append(soup.get_text())
+        contents.append(summarized_content)
 
     # Zip the lists together
     my_list = zip(titles, contents, image_urls)
